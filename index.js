3e7a362e982b4231a60550a1af7cd972ce9d1858
DipanshuPapreja <110007209+DipanshuPapreja@users.noreply.github.com>
2022-10-17 02:25:40 +0530

    Update index.js

--- index.js
+++ index.js
@@ -49,3 +49,14 @@ document.querySelector(".popup .close-btn").addEventListener("click", function (
 document.querySelector(".popup .button").addEventListener("click", function () {
     document.querySelector(".popup").classList.remove("active");
 });
+
+// ======================= Login Form Btn Click Come on Box ===========================
+
+let loginForm = document.querySelector('.login-form');
+
+document.querySelector('#login-btn').onclick = () =>{
+    loginForm.classList.toggle('active');
+    searchForm.classList.remove('active');
+    shoppingCart.classList.remove('active');
+    navbar.classList.remove('active');
+}
